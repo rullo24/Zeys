54ca06fddbf43283fb19578e622da40923b60a0f
Michael Wolf <susonicth@gmail.com>
2025-02-04 00:12:56 +0100

    made zeys fetachable

--- build.zig
+++ build.zig
@@ -2,18 +2,23 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const optimise = b.standardOptimizeOption(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
     const lib = b.addStaticLibrary(.{
         .name = "zeys",
         .root_source_file = b.path("./src/zeys.zig"),
         .target = target,
-        .optimize = optimise,
+        .optimize = optimize,
+    });
+
+    b.addModule("zeys", .{
+        .root_source_file = b.path("src/zeys.zig"),
+        .target = target,
+        .optimize = optimize,
     });
 
     b.reference_trace = 10;
     lib.linkSystemLibrary("user32"); // building Windows lib
 
     b.installArtifact(lib);
-}   
-
+}
